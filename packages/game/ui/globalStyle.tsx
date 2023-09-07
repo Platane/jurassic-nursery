5c8c9b21a4c993de2ebab3b5c05e027e74f045e4
platane <me@platane.me>
2023-09-07 23:07:04 +0200

    ✨ forbid pinch to zoom

--- packages/game/ui/globalStyle.tsx
+++ packages/game/ui/globalStyle.tsx
@@ -2,6 +2,11 @@ import { css } from "@linaria/core";
 
 css`
   :global() {
+    html,
+    body {
+      touch-action: none;
+    }
+
     html {
       font-family: Verdana, sans-serif;
       background-color: #f9f8ea;
